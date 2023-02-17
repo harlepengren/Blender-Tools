aadbb265f5b4b36a9f9c73f878f40cc6829aa380
kkoehler4070 <Koehler.62@gmail.com>
2023-02-17 05:00:08 -0500

    Camera
    Added camera rotation script

---
+++ camera.py
@@ -0,0 +1,34 @@
+#import the modules
+import bpy
+import numpy as np
+import math
+import mathutils
+ 
+# Create a variable to hold the camera object and the origin of the camera
+camera = bpy.data.objects['Camera']
+cameraOrigin = np.array(camera.location)
+
+# Create a variable to hold the focus object
+focusObject = bpy.data.objects['Cube']
+
+# Angle (in radians) that the camera will rotate each frame
+theta = 2*math.pi/250
+ 
+def rotateCamera(scene):
+    """Rotate the camera around an object. Takes the scene as parameter from handler."""
+
+
+    newTheta = theta*scene.frame_current
+    rotationMatrix = np.array([[math.cos(newTheta),-math.sin(newTheta),0],
+                                [math.sin(newTheta), math.cos(newTheta),0],
+                                [0,0,1]])
+    camera.location = np.dot(cameraOrigin,rotationMatrix) + focusObject.location
+     
+def setRotation():
+    """ Set handlers to call the rotateCamera function."""
+    # clear old handlers
+    bpy.app.handlers.frame_change_pre.clear()
+    #register a new handler
+    bpy.app.handlers.frame_change_pre.append(rotateCamera)
+     
+setRotation()
