d2baa4ab7d7c6682fbf9c49aa451bab44f4ef696
kkoehler4070 <koehler.62@gmail.com>
2023-07-12 20:00:33 -0400

    Update rendersettings.py
    Changed meta data and default renders (definitely don't need 30 renders).

--- rendersettings.py
+++ rendersettings.py
@@ -1,14 +1,14 @@
 bl_info = {
-    "name": "Camera Rotation",
+    "name": "Render Settings Text",
     "author": "Harlepengren",
     "version": (1, 0),
     "blender": (3, 4, 0),
     "location": "3D View Sidebar",
-    "description": "Rotates camera around a target object over a given number of frames.",
+    "description": "Creates renders with different settings to compare settings.",
     "warning": "",
     "doc_url": "",
     "tracker_url": "",
-    "category": "Object",
+    "category": "Scene",
 }
 
 #import the modules
@@ -16,18 +16,34 @@ import bpy
  
 # Property Group for the settings
 class RenderSettingsPropertyGroup(bpy.types.PropertyGroup):
-    numRenders: bpy.props.IntProperty(name="numRenders",default=30)
+    numRenders: bpy.props.IntProperty(name="numRenders",default=1)
     target: bpy.props.StringProperty(name="target", description="datapath of setting we want to change")
     start: bpy.props.FloatProperty(name="start", default=0.0, description="starting value")
     end: bpy.props.FloatProperty(name="end", default=1.0, description="ending value")
  
-class RenderSettingOperator(bpy.types.Operator):
+class RenderSettingsOperator(bpy.types.Operator):
     """Allows you to render multiple versions with different settings for comparison."""
     bl_idname = "scene.render_setting_operator"
     bl_label = "Render Instance Settings"
 
 
-    def execute(self, context):            
+    def execute(self, context):
+        print("Rendering the scene")
+
+        renderSlots = bpy.data.images["Render Result"].render_slots
+        
+        startingValue = context.scene.render_settings_prop.start
+        iterations = min(context.scene.render_settings_prop.numRenders,len(renderSlots))
+        incrementalValue = (context.scene.render_settings_prop.end - startingValue)/iterations
+        
+        for index in range(0,iterations):
+            renderSlots.active_index = index
+            
+            # Change the setting - using exec is not super safe
+            exec(context.scene.render_settings_prop.target + "=" + str(startingValue + incrementalValue*index))
+            
+            bpy.ops.render.render()
+        
         return {'FINISHED'}
      
 class RenderSettingsPanel(bpy.types.Panel):
